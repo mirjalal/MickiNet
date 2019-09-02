24be6b814cf2a37b851fecc59749975b74d90e23
mirjalal <mirjalal.talishinski@gmail.com>
2019-09-02 07:35:23 +0400

    1) Material design support added 2) Raleway font added & set as main font family 3) Tons of strings added 4) Dimens created 5) Migrated to AndroidX

--- app/src/main/kotlin/com/talmir/mickinet/WifiP2pStateChangeReceiver.kt
+++ app/src/main/kotlin/com/talmir/mickinet/WifiP2pStateChangeReceiver.kt
@@ -9,34 +9,34 @@ import android.net.wifi.p2p.WifiP2pManager
 class WifiP2pStateChangeReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        when(intent.action) {
-            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                // Determine if Wifi P2P mode is enabled or not, alert
-                // the Activity.
-                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
-                activity.isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
-            }
-            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
-
-                // The peer list has changed! We should probably do something about
-                // that.
-
-            }
-            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
-
-                // Connection state changed! We should probably do something about
-                // that.
-
-            }
-            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
-                (activity.supportFragmentManager.findFragmentById(R.id.frag_list) as DeviceListFragment)
-                    .apply {
-                        updateThisDevice(
-                            intent.getParcelableExtra(
-                                WifiP2pManager.EXTRA_WIFI_P2P_DEVICE) as WifiP2pDevice
-                        )
-                    }
-            }
-        }
+//        when(intent.action) {
+//            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+//                // Determine if Wifi P2P mode is enabled or not, alert
+//                // the Activity.
+//                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+//                activity.isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
+//            }
+//            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+//
+//                // The peer list has changed! We should probably do something about
+//                // that.
+//
+//            }
+//            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+//
+//                // Connection state changed! We should probably do something about
+//                // that.
+//
+//            }
+//            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+//                (activity.supportFragmentManager.findFragmentById(R.id.frag_list) as DeviceListFragment)
+//                    .apply {
+//                        updateThisDevice(
+//                            intent.getParcelableExtra(
+//                                WifiP2pManager.EXTRA_WIFI_P2P_DEVICE) as WifiP2pDevice
+//                        )
+//                    }
+//            }
+//        }
     }
 }
